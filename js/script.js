d84f16164f8e5ecea04e714e493bd5591becc457
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-23 15:06:59 +0100

    Create project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+    - Scrivi un programma che stampi in console i numeri da 1 a 100:
+    - I multipli di 3 devono avere come output “Fizz”
+    - I multipli di 5 devono avere come output “Buzz”
+    - I multipli sia di 3 che di 5 devono avere come output “FizzBuzz”
+CONSIGLI: 
+    - Scrivere i commenti in Italiano
+*/
+
+const elemento = document.getElementById("demo");
+
+for(let i=0 ; i<100; i++){
+    let numero;
+    numero++;
+    console.log(numero);
+    elemento.innerHTML += '
+    <p> ciao </p>
+    ';
+}
